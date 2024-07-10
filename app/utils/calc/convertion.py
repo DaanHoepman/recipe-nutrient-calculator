d1ab3e13128c9bd668fa73ae5480f626dcfd5fbb
DaanHoepman <daan.hoepman1508@gmail.com>
2024-07-10 14:27:17 +0200

    initializaiton for expansion

---
+++ app/utils/calc/convertion.py
@@ -0,0 +1,16 @@
+from app import db
+from app.models import Ingredient, Nutrition, Conversion
+
+#--------------------
+
+def convert_amount_db():
+    """
+
+    """
+    raise NotImplementedError()
+
+def convert_amount_API():
+    """
+
+    """
+    raise NotImplementedError()
---
+++ app/utils/calc/parse_ingredient.py
@@ -0,0 +1,28 @@
+from app import db
+from app.models import Ingredient, Nutrition, Conversion
+
+#--------------------
+
+def parse_ingredient():
+    """
+
+    """
+    raise NotImplementedError()
+
+def find_best_match_db():
+    """
+
+    """
+    raise NotImplementedError()
+
+def find_best_match_API():
+    """
+    
+    """
+    raise NotImplementedError()
+
+def extract_API_info():
+    """
+
+    """
+    raise NotImplementedError()
